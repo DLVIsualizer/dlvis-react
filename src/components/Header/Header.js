20b050a210921c5fdb610b4f1c5894991a30cc08
Jiwon Shin <jiwonshin@Jiwonui-MacBook-Pro.local>
2018-04-15 21:11:49 +0900

    react-materialize added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Header.css';
+import { Navbar, NavItem, Icon } from 'react-materialize';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <Navbar brand='logo' right>
+                    <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+                    <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+                    <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+                    <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default Header;
